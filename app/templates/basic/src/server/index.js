cf03172ed4c1dc01a16995e2ba950a4c84893729
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-09-05 08:46:57 -0500

    refactor index file

--- app/templates/basic/src/server/index.js
+++ app/templates/basic/src/server/index.js
@@ -6,57 +6,54 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
-function createApp() {
-  var app = express();
+var app = express();
 
-  app.engine('.html', require('ejs').__express);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'html');
+app.engine('.html', require('ejs').__express);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
 
-  app.set('title', '<%= applicationName %>');
+app.set('title', '<%= applicationName %>');
 
-  // app.use(favicon(__dirname + '/public/img/favicon.ico'));
-  app.use(logger('dev')); // log all requests
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser());
-  app.locals.bundle = require(path.join(__dirname, '../../bundle.result.json'));
+// app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(logger('dev')); // log all requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.locals.bundle = require(path.join(__dirname, '../../bundle.result.json'));
 
-  app.use('/public', express.static(path.join(__dirname, '../../public')));
-  app.use('/', routes);
+app.use('/public', express.static(path.join(__dirname, '../../public')));
+app.use('/', routes);
 
-  if (app.get('env') === 'development') {
-    app.locals.pretty = true;
-  }
-
-  /// catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-  // will print stacktrace
-  if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        error: err
-      });
-    });
-  } else {
-    // no stacktraces leaked to user
-    app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.render('error', {
-        error: err
-      });
-    });
-  }
-
-  return app;
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
 }
 
-module.exports = createApp;
+/// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      error: err
+    });
+  });
+} else {
+  // no stacktraces leaked to user
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      error: err
+    });
+  });
+}
+
+
+module.exports = app;
