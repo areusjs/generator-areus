2142437d3f847b7183db5b694e1b49bfa53d34d2
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-09-10 12:21:22 -0500

    refactor names to be consistent

--- app/templates/basic/src/lib/logger.js
+++ app/templates/basic/src/lib/logger.js
@@ -1,2 +1,3 @@
 var bunyan = require('bunyan');
+// naturally a singleton because node's require caches the value assigned to module.exports
 module.exports = bunyan.createLogger({name: '<%= _.slugify(applicationName) %>'});
--- app/templates/common/_server.js
+++ app/templates/common/_server.js
@@ -1,11 +1,11 @@
 var srcPath = './src';
-var log = require(srcPath + '/lib/logger');
+var logger = require(srcPath + '/lib/logger');
 var app = require(srcPath)();
-var properties = require(srcPath + '/lib/PropertyService').getProperties();
+var properties = require(srcPath + '/lib/property-service').getProperties();
 var port = properties.get('PORT');
 
 app.set('port', port);
 
 app.listen(port, function () {
-  log.info('Server listening on port %s', port);
+  logger.info('Server listening on port %s', port);
 });
