0444cc02fea51d9f92d69e818e290c2897852246
Chris Montgomery <christopher.montgomery@dowjones.com>
2014-11-21 13:13:56 -0600

    remove license from generated package.json

--- app/templates/react/src/public/scripts/app.js
+++ app/templates/react/src/public/scripts/app.js
@@ -6,16 +6,13 @@ var MyApp = require('../../components/MyApp.react.jsx');
 // Snag the initial state that was passed from the server side
 var stateDom = document.getElementById('initial-state');
 if (stateDom) {
-
   var initialStateString = stateDom.innerHTML;
   if (initialStateString) {
     var initialState = JSON.parse(initialStateString);
-
     // Render the components, picking up where react left off on the server
     React.renderComponent(
       <MyApp title={initialState.title} message={initialState.message}/>,
       document.getElementById('react-app')
     );
   }
-
 }
